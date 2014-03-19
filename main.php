7ae9197e7bf6b8bc58d326bb2afe28986eafef03
Mike Hall <mikehall314@gmail.com>
2014-03-19 09:57:39 +0000

    Update routing table for new Router

--- main.php
+++ main.php
@@ -15,16 +15,16 @@ require "escher.php";
 $router = new TDM\Escher\Router();
 
 // Set up the routing table
-$router->addStaticRoute('/', 'YourCompany\YourProject\Presenters\Homepage::request');
+$router->map('/', 'YourCompany\YourProject\Presenters\Homepage::request');
 
 // Identify how to route this request
-$route = $router->route($_SERVER["DOCUMENT_URI"], $_SERVER["REQUEST_METHOD"]);
+$route = $router->route($_SERVER["REQUEST_METHOD"], $_SERVER["DOCUMENT_URI"]);
 
 // If this request is not routable, call a 404
-if (!is_callable($route["callable"])) {
+if (!is_callable($route)) {
     TDM\Escher\HTTP\StatusCode::returnCode(404);
     exit;
 }
 
 // Pass the request on to the Presenter
-call_user_func_array($route["callable"], $route["arguments"]);
+call_user_func($route);
