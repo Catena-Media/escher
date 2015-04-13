1c7a27f88502df463d759eecdfa71fae74063ce8
Mike Hall <mike.hall@twistdigital.co.uk>
2015-04-13 12:11:51 +0100

    Main now echos by default

--- main.php
+++ main.php
@@ -20,11 +20,11 @@ $router->map('/', 'YourCompany\YourProject\Presenters\Homepage::request');
 // Identify how to route this request
 $route = $router->route($_SERVER["REQUEST_METHOD"], $_SERVER["DOCUMENT_URI"]);
 
-// If this request is not routable, call a 404
-if (!is_callable($route)) {
-    TDM\Escher\HTTP\StatusCode::returnCode(404);
-    exit;
-}
+// Route down to a presenter
+if (is_callable($route)) {
+    echo call_user_func($route);
 
-// Pass the request on to the Presenter
-call_user_func($route);
+} else {
+    // If this request is not routable, call a 404
+    TDM\Escher\HTTP\StatusCode::returnCode(404);
+}
